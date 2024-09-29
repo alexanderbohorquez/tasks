885adf36c4805ceff45a521fbf5764cf585f68ec
alexanderbohorquez <alexanderbohorquez96@gmail.com>
2024-09-29 18:32:57 -0500

    Primer commit: subir proyecto To-Do List

---
+++ app.js
@@ -0,0 +1,146 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if (storedTasks) {
+        storedTasks.forEach((task) => task.push(task));
+        updateTaskList();
+        updateStacks();
+    }
+});
+
+let task = [];
+
+const saveTasks = () => {
+    localStorage.setItem("tasks", JSON.stringify(task));
+};
+
+const addTask = () => {
+    const taskInput = document.getElementById("taskInput");
+    const text = taskInput.value.trim();
+
+    // Verificar si ya hay 5 tareas
+    if (task.length >= 5) {
+        alert("No puedes agregar más de 5 tareas.");
+        return; // Salir de la función si ya hay 5 tareas
+    }
+
+    if (text) {
+        task.push({ text: text, completed: false });
+        taskInput.value = "";
+        updateTaskList();
+        updateStacks();
+        saveTasks();
+    }
+};
+
+const toggleTaskComplete = (index) => {
+    task[index].completed = !task[index].completed;
+    updateTaskList();
+    updateStacks();
+    saveTasks();
+};
+
+const deleteTask = (index) => {
+    task.splice(index, 1);
+    updateTaskList();
+    updateStacks();
+    saveTasks();
+};
+
+const editTask = (index) => {
+    const taskInput = document.getElementById("taskInput");
+    taskInput.value = task[index].text;
+
+    task.splice(index, 1);
+    updateTaskList();
+    updateStacks();
+    saveTasks();
+};
+
+const updateStacks = () => {
+    const completeTasks = task.filter((task) => task.completed).length;
+    const totalTasks = task.length;
+    const progress = (completeTasks / totalTasks) * 100 || 0; // Evitar NaN si totalTasks es 0
+    const progressBar = document.getElementById("progress");
+
+    progressBar.style.width = `${progress}%`;
+
+    document.getElementById(
+        "numbers"
+    ).innerText = `${completeTasks}/${totalTasks}`;
+
+    if(task.length && completeTasks === totalTasks) {
+        blaskConfetti()
+    }
+};
+
+const updateTaskList = () => {
+    const taskList = document.getElementById("task-list");
+    taskList.innerHTML = "";
+
+    task.forEach((task, index) => {
+        const listItem = document.createElement("li");
+
+        listItem.innerHTML = `
+        <div class="taskItem">
+            <div class="task ${task.completed ? "completed" : ""}">
+                <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""
+            } />
+                <img src="IMG/Done.png" alt="Checked" class="check-image" style="display: none;" />
+                <p>${task.text}</p>
+            </div>
+            <div class="icons">
+                <div class="icon-container">
+                    <img src="IMG/Edit.png" onClick="editTask(${index})" />
+                </div>
+                <div class="icon-container">
+                    <img src="IMG/Delete.png" onClick="deleteTask(${index})" />
+                </div>
+            </div>
+        </div>
+        `;
+
+        // Agregar el evento de cambio al checkbox
+        const checkbox = listItem.querySelector(".checkbox");
+        checkbox.addEventListener("change", () => toggleTaskComplete(index));
+
+        taskList.append(listItem);
+    });
+};
+
+document.getElementById("newTask").addEventListener("click", function (e) {
+    e.preventDefault();
+    addTask();
+});
+
+const blaskConfetti = () => {
+    const defaults = {
+        spread: 360,
+        ticks: 50,
+        gravity: 0,
+        decay: 0.94,
+        startVelocity: 30,
+        shapes: ["star"],
+        colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
+    };
+
+    function shoot() {
+        confetti({
+            ...defaults,
+            particleCount: 40,
+            scalar: 1.2,
+            shapes: ["star"],
+        });
+
+        confetti({
+            ...defaults,
+            particleCount: 10,
+            scalar: 0.75,
+            shapes: ["circle"],
+        });
+    }
+
+    setTimeout(shoot, 0);
+    setTimeout(shoot, 100);
+    setTimeout(shoot, 200);
+};
